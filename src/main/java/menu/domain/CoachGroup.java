a4e56c7d7ba1b1f31cf3b6bae9ec885245570708
sinminseok12 <sinminseok12@naver.com>
2024-12-12 13:40:16 +0900

    feat(CoachGroup) : 코치 인원 검증 기능 구현

--- src/main/java/menu/domain/CoachGroup.java
+++ src/main/java/menu/domain/CoachGroup.java
@@ -4,17 +4,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CoachGroup {
+    private static final Integer MIN_COACH_GROUP_RANGE = 2;
+    private static final Integer MAX_COACH_GROUP_RANGE = 5;
     private final List<Coach> coaches;
 
     private CoachGroup(final List<Coach> coaches) {
         this.coaches = coaches;
     }
 
-    public static CoachGroup from(final List<String> names){
+    public static CoachGroup from(final List<String> names) {
+        validateCoachGroupRange(names);
         List<Coach> collect = names.stream()
                 .map(Coach::from)
                 .collect(Collectors.toList());
         return new CoachGroup(collect);
+    }
+
+    private static void validateCoachGroupRange(final List<String> names) {
+        if(names.size() < MIN_COACH_GROUP_RANGE || names.size() > MAX_COACH_GROUP_RANGE);
     }
 
     public List<Coach> getCoaches() {
