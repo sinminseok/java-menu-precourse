58b7722c2dbaf02f3fdb2b1104a74572f8a0d8ed
sinminseok12 <sinminseok12@naver.com>
2024-12-12 11:37:39 +0900

    feat(InputView) : 각 코치가 못 먹는 메뉴 입력 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,6 +6,7 @@ import menu.global.utils.Converter;
 import java.util.List;
 
 import static menu.global.constants.ViewMessage.INPUT_COACH_NAMES_MESSAGE;
+import static menu.global.constants.ViewMessage.INPUT_PICKY_EATING_MESSAGE;
 
 public class InputView {
 
@@ -16,6 +17,12 @@ public class InputView {
         System.out.println(INPUT_COACH_NAMES_MESSAGE);
         String input = Console.readLine();
         //todo 검증
+        return Converter.splitDelimiter(input);
+    }
+
+    public static List<String> inputPickyEating(String coachName){
+        System.out.println(coachName + INPUT_PICKY_EATING_MESSAGE);
+        String input = Console.readLine();
         return Converter.splitDelimiter(input);
     }
 }
